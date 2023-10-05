886933314f5bb5d112db00b3ab56758a8de45f1f
Ashish19071998 <144302293+Ashish19071998@users.noreply.github.com>
2023-10-05 13:48:53 +0530

    Updates

---
+++ Day-1-JS/Day-1-JS.js
@@ -0,0 +1,76 @@
+// Datatypes  
+
+
+
+// String
+// Syntax -> "" , '' , ``
+
+console.log(typeof("Kuchbhi bolna hai"))
+console.log(typeof('Testing of single coats'))
+console.log(typeof(`Back ticks`))
+console.log(typeof("8888118633"))
+console.log(typeof("true"))
+
+
+
+
+// Number
+// Syntax -> 8888118633
+
+
+console.log(typeof(8888118633))
+
+
+
+
+// Boolean
+// Syntax -> true / false
+
+console.log(typeof(true))
+
+
+
+// Variables
+
+// var
+// let
+// const
+
+
+// var
+
+var ashish = 885644
+console.log(ashish)
+console.log(typeof(ashish))
+
+var irise = "Hello Students!"
+console.log(irise)
+console.log(typeof(irise))
+
+var akash = true;     // true / false 
+console.log(akash)
+console.log(typeof(akash))
+
+
+
+// Operations
+
+var number1 = 23;
+var number2 = 100;
+var addition = number1 + number2;
+console.log("Addition", addition)
+
+var number3 = 100;
+var number4 = 50;
+var sub = number3 - number4;
+console.log("Subtraction", sub)
+
+var number5 = 100;
+var number6 = 3;
+var multi = number5 * number6;
+console.log("Multiplication", multi)
+
+var number7 = 120;
+var number8 = 2;
+var div = number7 / number8;
+console.log("Division", div)
