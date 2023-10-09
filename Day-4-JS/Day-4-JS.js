7fd72b758ceb1e435a73d1b9f0fab537da952655
Ashish19071998 <144302293+Ashish19071998@users.noreply.github.com>
2023-10-09 16:40:59 +0530

    Updates

---
+++ Day-4-JS/Day-4-JS.js
@@ -0,0 +1,39 @@
+//  For loop  
+
+
+
+// for(var i=0;i<=10;i++){
+//     console.log(i)
+// }
+
+
+
+// for(var i=10;i>=1;i--){
+//     console.log(i)
+// }
+
+// ----------------------------------------------------------------------
+
+//Function
+
+// function anyName (){
+//     console.log("hi")
+// }
+// anyName();
+
+
+// function ashish(number){
+//     console.log(number + number)
+// }
+// ashish(2);
+
+
+// function ashish(number1, number2){
+//     console.log(number1 + number2)
+//     console.log(number1 - number2)
+//     console.log(number1 * number2)
+//     console.log(number1 / number2)
+//     console.log(number1 % number2)
+// }
+// ashish(12,2);
+
