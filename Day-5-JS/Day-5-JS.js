a1a3bf3a61bbef9b9ffa72ac852e98396b5b02c9
Ashish19071998 <144302293+Ashish19071998@users.noreply.github.com>
2023-10-10 09:45:19 +0530

    Updates

---
+++ Day-5-JS/Day-5-JS.js
@@ -0,0 +1,17 @@
+
+var students = ['Akash', 'Avinash', 'Shubham', 'Rajesh'];
+
+for (var i = 0; i < students.length; i++) {
+
+    console.log(students[i])
+}
+
+
+function ashish(num1, num2) {
+    console.log("Addition is =", num1 + num2)
+    console.log("Subtraction is =", num1 - num2)
+    console.log("Multiplication is =", num1 * num2)
+    console.log("Division is =", num1 / num2)
+    console.log("Module is =", num1 % num2)
+}
+ashish(12, 2);
